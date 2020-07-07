252dd4e7f94298c0d858366827ad8ccabc4174e3
Ybrayym <ybrayymjan90@gmail.com>
2020-07-07 11:31:30 -0400

    added all of the apps

--- REDDIT_CLONE/settings.py
+++ REDDIT_CLONE/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # our apps
+    'authentication',
+    'comments',
+    'posts',
+    'subreddit',
 ]
 
 MIDDLEWARE = [
