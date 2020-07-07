b62a925b04f01020533ff73beb6e0f27ab72d9da
Janelle Kuhns <j3ckuhns@gmail.com>
2020-07-07 08:47:49 -0700

    updated models

--- authentication/models.py
+++ authentication/models.py
@@ -1,3 +1,13 @@
 from django.db import models
-
+from django.contrib.auth.models import AbstractUser
+from subreddit.models import SubReddit
 # Create your models here.
+
+class RedditUser(AbstractUser):
+    karma = models.IntegerField(default=0)
+
+
+class Moderator(models.Model):
+    is_moderator = models.BooleanField(default=False)
+    user = models.ForeignKey(RedditUser, on_delete=models.CASCADE)
+    subreddit = models.ForeignKey(SubReddit, on_delete=models.CASCADE)
