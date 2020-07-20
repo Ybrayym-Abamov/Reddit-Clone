0dc01032d9c2bed746b9b1729d0ccba727536984
Ybrayym <ybrayymjan90@gmail.com>
2020-07-20 19:50:00 -0400

    subreddits on main page, subreddits count and styling

--- subreddit/urls.py
+++ subreddit/urls.py
@@ -1,9 +1,10 @@
 from django.urls import path
-from subreddit.views import add_subreddit, subredditview
-
+from subreddit.views import add_subreddit, subredditview, subreddithot, subredditnew
 
 
 urlpatterns = [
     path('addsubreddit/', add_subreddit, name='addsubreddit'),
-    path('r/<str:name>/', subredditview, name='subreddit')
+    path('r/<str:name>/', subredditview, name='subreddit'),
+    path('r/<str:name>/new/', subredditnew, name='subredditnew'),
+    path('r/<str:name>/hot/', subreddithot, name='subreddithot')
 ]
