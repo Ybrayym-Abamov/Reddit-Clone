c027f291bfe73a50318d9c1c3d8642f45d812417
Ybrayym <ybrayymjan90@gmail.com>
2020-07-21 20:37:30 -0400

    added some styling, added the delete feature to the comments

--- comments/urls.py
+++ comments/urls.py
@@ -5,5 +5,5 @@ from comments.views import up_vote, down_vote, delete_view
 urlpatterns = [
     path('r/<str:name>/post/<int:id>/upvote/<int:id2>/', up_vote, name='comment_upvote'),
     path('r/<str:name>/post/<int:id>/downvote/<int:id2>/', down_vote, name='comment_downvote'),
-    path('delete/<int:id>/', delete_view, name='delete')
+    path('delete_comment/<int:id>/', delete_view, name='delete_comment')
 ]
