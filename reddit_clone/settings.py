79c4e9e42f71fde1d9b08b882c6fef578db11bfa
Ybrayym <ybrayymjan90@gmail.com>
2020-08-22 23:23:39 -0400

    restart deployment process

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -11,7 +11,6 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
-import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -132,6 +131,3 @@ STATIC_URL = '/static/'
 AUTH_USER_MODEL = 'authentication.RedditUser'
 
 LOGIN_URL = '/login/'
-
-# Activate Django-Heroku.
-django_heroku.settings(locals())
