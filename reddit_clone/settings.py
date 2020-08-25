d65ecf29baa7e4138d3a9038ecb93f1dc39b4b99
Ybrayym <ybrayymjan90@gmail.com>
2020-08-25 09:43:57 -0400

    added the dj-django-url into the requirements.txt file

--- reddit_clone/settings.py
+++ reddit_clone/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 """
 
 import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -127,6 +128,10 @@ USE_TZ = True
 
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
+
+db_from_env = dj_database_url.config(conn_max_age=500)
+
+DATABASES['default'].update(db_from_env)
 
 STATIC_URL = '/static/'
 
